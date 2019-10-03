83d53d61574bbdeb243084e84cd706c4829dbafc
Andres Gesteira <gesteira.pro@gmail.com>
2019-10-03 21:09:29 +0200

    add angular material

--- projects/handwerker-jobs-app/src/app/app.module.ts
+++ projects/handwerker-jobs-app/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   bootstrap: [AppComponent],
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule]
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule]
 })
 export class AppModule {}
