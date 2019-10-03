d89808ee58a1e9c26805bae16027efff2d66f1f2
Andres Gesteira <gesteira.pro@gmail.com>
2019-10-03 21:06:23 +0200

    missing configs

--- projects/handwerker-jobs-app/src/app/app.component.ts
+++ projects/handwerker-jobs-app/src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'hdj-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'handwerker-jobs-app';
